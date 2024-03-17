495fba99ddc44c0b07cd6f04a208a33d97a71f79
Scrimali Anthony <anthony.scrimali@gmail.com>
2024-03-17 13:22:58 +0100

    Cours Terminé

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">Coffee Card</h1>
+
+Coffee Card est un projet d'apprentissage du langage Flutter par Net Ninja.
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:coffee_card/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: const Text('Sandbox'), backgroundColor: Colors.grey),
+      body: Row(children: [
+        Container(
+          height: 100,
+          color: Colors.red,
+          child: const Text('One'),
+        ),
+        Container(
+          height: 200,
+          color: Colors.green,
+          child: const Text('Two'),
+        ),
+        Container(
+          height: 300,
+          color: Colors.blue,
+          child: const Text('Tree'),
+        )
+      ]),
+    );
+  }
+}
